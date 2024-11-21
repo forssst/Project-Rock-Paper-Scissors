e7afa78baa0402c262859f7d742e991605b9ae3d
Forst <szymonforstkl3trgwitam@gmail.com>
2024-11-21 16:02:54 +0100

    remove console logs and clean up code

--- script.js
+++ script.js
@@ -1,15 +1,12 @@
-console.log('Hello!')
-console.log('If you want to play a Rock Paper Scissors game type \'playGame()\'')
+
 
 let playerScore = 0;
 let computerScore = 0;
 const scoreDiv = document.querySelector(".scoreDiv");
 const compChoice = document.querySelector(".compChoice");
 const whoWonDiv = document.querySelector(`.whoWonDiv`);
+const choiceButDiv = document.querySelector(".choiceButtons");
 
-console.log(whoWonDiv)
-
-// to ma zwracac string xdd
 
 function playerChoice (pickNum) {
     pickNum;
@@ -20,7 +17,6 @@ function playerChoice (pickNum) {
    } else if (pickNum === 3){
         pickNum = 'Scissors'
    } else {
-    console.log('you picked wrong number so you get a rock')
     pickNum = 'Rock'
    }
 
@@ -47,7 +43,6 @@ function playRound(pick) {
 
     computer = computerChoice().toLowerCase();
 if (player == 'rock' ) {
-    console.log('player picked Rock !')
     if (player  === computer) {
         compChoice.textContent = ('computer picked Rock !')
         playerScore += 1
@@ -65,7 +60,6 @@ if (player == 'rock' ) {
         whoWonDiv.textContent =('Something went wrong sorry :(')
     }
 } else if (player == 'paper') {
-        console.log('player picked Paper !')
         if (player === computer) {
             compChoice.textContent = ('computer picked Paper !')
             playerScore += 1
@@ -83,7 +77,6 @@ if (player == 'rock' ) {
             whoWonDiv.textContent =('Something went wrong sorry :(')
         }
 } else if (player == 'scissors') {
-    console.log('player picked Scissors !')
         if (player === computer) {
             compChoice.textContent = ('computer picked Scissors !')
             playerScore += 1
@@ -127,13 +120,6 @@ scoreDiv.textContent = `the score is ${playerScore} - ${computerScore}`;
 
 }
 
-/// dokoczyc zeby kazdy komunikat sie pojawial w domie to kot wygyrqa i wgl punkt 2/3
-
-
-
-const choiceButDiv = document.querySelector(".choiceButtons");
-
-console.log(choiceButDiv)
 
 choiceButDiv.addEventListener("click", (e) => {
     console.log(e.target.textContent)
@@ -148,18 +134,3 @@ choiceButDiv.addEventListener("click", (e) => {
     } else {playRound(1)}
 });
 
-
-
-checkScorePlayer =+ playerScore;
-console.log(checkScorePlayer)
-
-
-
-
-
-// function playGame(howManyGames) {
-// howManyGames = +prompt('How many times do you want to play?(default is 5)', 5);
-// for (let i = 0; i < howManyGames; i++) {
-// playRound()
-// }
-// }
